1bf4fdeebf022e097f61b892ac5e2103b378bfa7
Dorjsuren <dorjoo3013@gmail.com>
2024-01-03 06:35:51 +0800

    about dispense change

--- src/components/poster/dispenseInfo.tsx
+++ src/components/poster/dispenseInfo.tsx
@@ -5,39 +5,38 @@ import { IconHandler } from "../../utils/icon"
 import Image from "next/image"
 
 export const DispenseInfo = () => {
-    return <SafeAreaSection color="linear-gradient(180deg, #DFF8BE 0%, #C2F486 100%)" classname="py-20 m:py-[100px] relative overflow-hidden">
-        <FlexColumn className="items-center gap-[90px] m:gap-[55px] pb-16 m:pb-0">
-            <FlexColumn className="gap-[75px] m:gap-[100px] z-20">
+    return <SafeAreaSection color="linear-gradient(180deg, #DFF8BE 0%, #C2F486 100%)" classname="pt-20 m:py-20 xl:py-[100px] relative overflow-hidden">
+        <FlexColumn className="items-center  pb-16 m:pb-0 gap-4 m:gap-0">
+            <FlexColumn className="gap-8 z-20">
                 <FlexColumn className="items-center gap-[16px]">
                     <Typography intent={"mons16"} classname="uppercase tracking-[3.76px] text-text-black-100/40 text-center">
                         About us
                     </Typography>
-                    <FlexColumn className="gap-6 items-center">
-                        <Typography intent={"headingGrstk"} classname="text-center text-[36px] m:text-[48px]">
-                            What is <br /> Dispense?
-                        </Typography>
-                        <Typography classname="m:w-[616px] font-sans text-[15px] leading-[22.5px] font-normal text-text-black-70 text-center">
-                            Unlike a typical vending machine, the whole purchasing experience is cashless and touchless. By just obtaining a QR code after a successful purchase, you can now pick up your items to over 192 total dispensary all over the state.
-                        </Typography>
-                        <Button intent={"filled"} text="LEARN MORE" classname="w-full m:w-[229px]" typographyVariant="buttons" icon={<IconHandler name="arrow-right" classname={["font-light tracking-[2.03px]"].join(" ")} />} />
-                    </FlexColumn>
+
+                    <Typography classname="m:w-[616px] font-sans text-[15px] leading-[22.5px] font-normal text-text-black-70 text-center">
+                        Dispense streamlines operations, and creates an alternative shopping experience for customers.
+                    </Typography>
+
                 </FlexColumn>
                 <FlexColumn className="items-center gap-[16px]">
                     <Typography intent={"mons16"} classname="uppercase tracking-[3.76px] text-text-black-100/40 text-center">
-                        About us
+                        hOW IT WORKS
                     </Typography>
-                    <FlexColumn className="gap-6 items-center">
-                        <Typography intent={"headingGrstk"} classname="text-center text-[36px] m:text-[48px]">
-                            How does <br /> Dispense work?
-                        </Typography>
-                        <Typography classname="m:w-[385px] font-sans text-[15px] leading-[22.5px] font-normal text-text-black-70 text-center">
-                            In just three easy steps, you can now grab your products from any dispensaries near you.
-                        </Typography>
-                    </FlexColumn>
+                    <Typography classname="m:max-w-[616px] font-sans text-[15px] leading-[22.5px] font-normal text-text-black-70 text-center">
+                        Licensed cannabis vendors display products on DispenseGO which are available instantly, or can also be picked up anytime in the next 24-hours.
+                    </Typography>
+                    <Typography classname="m:max-w-[616px] font-sans text-[15px] leading-[22.5px] font-normal text-text-black-70 text-center">
+                        The unique QR code given in every receipt is used at the location by a Dispense machine that reads the QR receipt and instantly dispenses the order.
+                    </Typography>
+                    <Typography classname="m:max-w-[616px] font-sans text-[15px] leading-[22.5px] font-normal text-text-black-70 text-center">
+                        Take time to browse selections from multiple dispensaries in your area or anywhere.  Pick your order up wherever you want with no line.  Enjoy a contactless, cashless, efficient in-store experience.
+                    </Typography>
                 </FlexColumn>
+                <Button intent={"filled"} text="LEARN MORE" classname="w-full m:w-[229px] self-center mt-2" typographyVariant="buttons" icon={<IconHandler name="arrow-right" classname={["font-light tracking-[2.03px]"].join(" ")} />} />
             </FlexColumn>
-            <Image alt="dispense" width={1114} height={732} className="hidden m:block" src={"assets/vending.svg"} />
-            <Image alt="dispense" width={400} height={400} className="scale-150 m:hidden" src={"/assets/Frame 34.svg"} />
+            <Image alt="dispense" width={1114} height={732} className="hidden xl:block" src={"assets/vending.svg"} />
+            <Image alt="dispense" width={696} height={653} className="hidden m:block xl:hidden" src={"/assets/vending-tab.svg"} />
+            <Image alt="dispense" width={400} height={400} className=" m:hidden" src={"/assets/Frame 34.svg"} />
         </FlexColumn>
         <Image alt="canabis" width={96} height={70} src={"assets/canabis.svg"} className="absolute top-[20px] left-[60px] m:top-[50px] m:left-[60px] xl:top-[184px] xl:left-[96px] z-10 " />
         <Image alt="canabis" width={96} height={70} src={"assets/canabis.svg"} className="absolute top-[832px] left-[55px] z-10 -rotate-45 hidden xl:block" />
