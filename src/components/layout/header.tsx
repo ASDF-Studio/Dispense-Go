c329ebfe3490f8df1f683dd1ab5abf4b74803bf4
Dorjsuren <dorjoo3013@gmail.com>
2024-01-04 03:32:24 +0800

    update

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -96,7 +96,7 @@ export const Header: FC<HeaderProps> = ({ withSearchBar }) => {
                     </Flex>
                 </FlexColumn>
             </SafeScreen>
-            <LoginModal open={showModal} setOpen={setShowModal} />
+            {/* <LoginModal open={showModal} setOpen={setShowModal} /> */}
             <Drawer open={showDrawer} setOpen={setShowDrawer} />
         </header>
     );
