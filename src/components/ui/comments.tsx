033554fadfa638f8738afd8dd24babfd106d6dc9
ss joy <sss.joy.one@gmail.com>
2024-04-17 20:58:30 +0600

    add static data for customer reviews and fix rating calculation

--- src/components/ui/comments.tsx
+++ src/components/ui/comments.tsx
@@ -6,106 +6,124 @@ import { faStar as RegularStar } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconHandler } from "../../utils/icon";
 import { SafeAreaSection } from "@/layout/spacing";
+import { type FC } from "react";
+import { singleProductDetails } from "../../static-data/single-product-details";
 
-const CommentCard = () => {
-    return (
-        <FlexColumn className="p-6 rounded-md border border-border-whiteSmoke bg-white gap-5 shadow-navbar">
-            <Flex className="justify-between">
-                <Flex className="gap-0.5">
-                    {Array(5)
-                        .fill(0)
-                        .map((_, index) => {
-                            return (
-                                <FontAwesomeIcon
-                                    key={index}
-                                    icon={index + 1 < 4 ? faStar : RegularStar}
-                                    className={[
-                                        `fas fa-star text-[14px] font-extrabold tracking-[2.03px]  leading-[14px]`,
-                                        "text-primary-brand",
-                                    ].join(" ")}
-                                />
-                            );
-                        })}
-                </Flex>
-                <Flex>
-                    {/* <AccountIcon/> */}
-                    <Typography
-                        intent={"mons15"}
-                        classname="leading-[19.5px] font-normal"
-                    >
-                        San P. <Typography classname="text-text-black-40">|</Typography> Oct
-                        04, 2022
-                    </Typography>
-                </Flex>
-            </Flex>
-            <Typography
-                classname="leading-[23.4px] font-semibold text-primary-black"
-                intent={"mons18"}
-            >
-                Wake and bake for an amazing and sunny day, no matter the weather.
-            </Typography>
-            <Typography
-                classname="leading-[22.5px] font-normal text-primary-black"
-                intent={"mons15"}
-            >
-                Strain: Super Lemon Haze Type: Sativa-dominant (80% Sativa) Genetics:
-                Lemon Skunk x Super Silver Haze Notes: Spicy lemon smell with hazy,
-                wispy nugs covered in hairs. Slow onset that starts with a light
-                excitement/euphoria and a defined head high. Very focused and cerebral,
-                giggly, and uplifted. Great for daytime and going out. Defined paranoia
-                in certain situations, as well as heavy munchies. After the peak, a mild
-                and energetic come down that lasts a long time. Great for depression and
-                lethargy though not necessarily for anxiety or stress. 8/10
-            </Typography>
-            <Button
-                intent={"text"}
-                text="Report"
-                className="self-end"
-                textClassname="font-normal leading-[18px] underline text-primary-black"
-                typographyVariant="mons15"
-            />
-        </FlexColumn>
-    );
+type CommentCardProps = {
+  reviewText: string;
+  reviewHeader: string;
+  reviewDate: string;
+  reviewRating: number;
+  reviewerName: string;
+};
+
+const CommentCard: FC<CommentCardProps> = ({
+  reviewText,
+  reviewHeader,
+  reviewDate,
+  reviewRating,
+  reviewerName,
+}) => {
+  return (
+    <FlexColumn className="p-6 rounded-md border border-border-whiteSmoke bg-white gap-5 shadow-navbar">
+      <Flex className="justify-between">
+        <Flex className="gap-0.5">
+          {Array(5)
+            .fill(0)
+            .map((_, index) => {
+              return (
+                <FontAwesomeIcon
+                  key={index}
+                  icon={index + (4 - reviewRating) < 4 ? faStar : RegularStar}
+                  className={[
+                    `fas fa-star text-[14px] font-extrabold tracking-[2.03px]  leading-[14px]`,
+                    "text-primary-brand",
+                  ].join(" ")}
+                />
+              );
+            })}
+        </Flex>
+        <Flex>
+          {/* <AccountIcon/> */}
+          <Typography
+            intent={"mons15"}
+            classname="leading-[19.5px] font-normal"
+          >
+            {reviewerName}{" "}
+            <Typography classname="text-text-black-40">|</Typography>{" "}
+            {reviewDate}
+          </Typography>
+        </Flex>
+      </Flex>
+      <Typography
+        classname="leading-[23.4px] font-semibold text-primary-black"
+        intent={"mons18"}
+      >
+        {reviewHeader}
+      </Typography>
+      <Typography
+        classname="leading-[22.5px] font-normal text-primary-black"
+        intent={"mons15"}
+      >
+        {reviewText}
+      </Typography>
+      <Button
+        intent={"text"}
+        text="Report"
+        className="self-end"
+        textClassname="font-normal leading-[18px] underline text-primary-black"
+        typographyVariant="mons15"
+      />
+    </FlexColumn>
+  );
 };
 
 export const Comments = () => {
-    return (
-        <SafeAreaSection>
-            <FlexColumn>
-                <div className="h-[1px] bg-border-whiteSmoke" />
-                <FlexColumn className="max-w-screen-margin mx-auto w-full gap-6 pt-[35px] xl:pt-[47px] pb-12 xl:pb-[142px]">
-                    <FlexColumn className="gap-2.5">
-                        <Typography
-                            intent={"grstk24"}
-                            classname="uppercase leading-[24px] tracking-[-0.84px] font-medium text-black"
-                        >
-                            Customer reviews
-                        </Typography>
-                        <Rating
-                            textColor="black"
-                            starColor="black"
-                            rating={4}
-                            count={212}
-                            extend
-                        />
-                    </FlexColumn>
-                    <Flex className="gap-2 justify-end">
-                        <Typography intent={"mons15"} classname="font-normal leading-[19.5px] text-primary-black"> Sort by <Typography classname="font-bold">
-                            Newest First
-                        </Typography></Typography>
-                        <IconHandler name="chevrondown" />
-                    </Flex>
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                </FlexColumn>
-            </FlexColumn>
-        </SafeAreaSection>
-    );
+  return (
+    <SafeAreaSection>
+      <FlexColumn>
+        <div className="h-[1px] bg-border-whiteSmoke" />
+        <FlexColumn className="max-w-screen-margin mx-auto w-full gap-6 pt-[35px] xl:pt-[47px] pb-12 xl:pb-[142px]">
+          <FlexColumn className="gap-2.5">
+            <Typography
+              intent={"grstk24"}
+              classname="uppercase leading-[24px] tracking-[-0.84px] font-medium text-black"
+            >
+              Customer reviews
+            </Typography>
+            <Rating
+              textColor="black"
+              starColor="black"
+              rating={singleProductDetails.customerReviews.overallRating + 1}
+              count={singleProductDetails.customerReviews.totalReviewCount}
+              extend
+            />
+          </FlexColumn>
+          <Flex className="gap-2 justify-end">
+            <Typography
+              intent={"mons15"}
+              classname="font-normal leading-[19.5px] text-primary-black"
+            >
+              {" "}
+              Sort by{" "}
+              <Typography classname="font-bold">Newest First</Typography>
+            </Typography>
+            <IconHandler name="chevrondown" />
+          </Flex>
+          {singleProductDetails.customerReviews.reviews.map((review) => {
+            return (
+              <CommentCard
+                key={review.reviewId}
+                reviewHeader={review.reviewHeading}
+                reviewText={review.reviewText}
+                reviewDate={review.reviewDate}
+                reviewRating={review.reviewStars}
+                reviewerName={review.reviewerName}
+              />
+            );
+          })}
+        </FlexColumn>
+      </FlexColumn>
+    </SafeAreaSection>
+  );
 };
---
+++ src/static-data/single-product-details.ts
@@ -0,0 +1,108 @@
+export const singleProductDetails = {
+  productDetails: {},
+  customerReviews: {
+    overallRating: 3,
+    totalReviewCount: 212,
+    reviews: [
+      {
+        reviewId: "3fa06ee9-ce61-5b9f-a250-d9f3280b0474",
+        reviewHeading:
+          "Wake and bake for an amazing and sunny day, no matter the weather.",
+        reviewText: `
+        Strain: Super Lemon Haze Type: Sativa-dominant (80% Sativa) Genetics: Lemon Skunk x Super Silver Haze Notes: Spicy lemon smell with hazy, wispy nugs covered in hairs. Slow onset that starts with a light excitement/euphoria and a defined head high. Very focused and cerebral, giggly, and uplifted. Great for daytime and going out. Defined paranoia in certain situations, as well as heavy munchies. After the peak, a mild and energetic come down that lasts a long time. Great for depression and lethargy though not necessarily for anxiety or stress. 8/10
+        `,
+        reviewDate: "Oct 04, 2022",
+        reviewerName: "San P.",
+        reviewStars: 5,
+      },
+      {
+        reviewId: "595dc9b8-31bc-57e8-879d-b74bc2327c94",
+        reviewHeading:
+          "Wake and bake for an amazing and sunny day, no matter the weather.",
+        reviewText: `
+        Strain: Super Lemon Haze Type: Sativa-dominant (80% Sativa) Genetics: Lemon Skunk x Super Silver Haze Notes: Spicy lemon smell with hazy, wispy nugs covered in hairs. Slow onset that starts with a light excitement/euphoria and a defined head high. Very focused and cerebral, giggly, and uplifted. Great for daytime and going out. Defined paranoia in certain situations, as well as heavy munchies. After the peak, a mild and energetic come down that lasts a long time. Great for depression and lethargy though not necessarily for anxiety or stress. 8/10
+        `,
+        reviewDate: "Oct 04, 2022",
+        reviewerName: "San P.",
+        reviewStars: 4,
+      },
+      {
+        reviewId: "e540f073-dbe9-5705-8321-513ecc12fd34",
+        reviewHeading:
+          "Wake and bake for an amazing and sunny day, no matter the weather.",
+        reviewText: `
+        Strain: Super Lemon Haze Type: Sativa-dominant (80% Sativa) Genetics: Lemon Skunk x Super Silver Haze Notes: Spicy lemon smell with hazy, wispy nugs covered in hairs. Slow onset that starts with a light excitement/euphoria and a defined head high. Very focused and cerebral, giggly, and uplifted. Great for daytime and going out. Defined paranoia in certain situations, as well as heavy munchies. After the peak, a mild and energetic come down that lasts a long time. Great for depression and lethargy though not necessarily for anxiety or stress. 8/10
+        `,
+        reviewDate: "Oct 04, 2022",
+        reviewerName: "San P.",
+        reviewStars: 5,
+      },
+      {
+        reviewId: "f3d8d08c-f60e-5879-9169-8bc83ae6e40d",
+        reviewHeading:
+          "Wake and bake for an amazing and sunny day, no matter the weather.",
+        reviewText: `
+        Strain: Super Lemon Haze Type: Sativa-dominant (80% Sativa) Genetics: Lemon Skunk x Super Silver Haze Notes: Spicy lemon smell with hazy, wispy nugs covered in hairs. Slow onset that starts with a light excitement/euphoria and a defined head high. Very focused and cerebral, giggly, and uplifted. Great for daytime and going out. Defined paranoia in certain situations, as well as heavy munchies. After the peak, a mild and energetic come down that lasts a long time. Great for depression and lethargy though not necessarily for anxiety or stress. 8/10
+        `,
+        reviewDate: "Oct 04, 2022",
+        reviewerName: "San P.",
+        reviewStars: 1,
+      },
+      {
+        reviewId: "891726fc-1bd5-558d-9982-f1f539ae1de5",
+        reviewHeading:
+          "Wake and bake for an amazing and sunny day, no matter the weather.",
+        reviewText: `
+        Strain: Super Lemon Haze Type: Sativa-dominant (80% Sativa) Genetics: Lemon Skunk x Super Silver Haze Notes: Spicy lemon smell with hazy, wispy nugs covered in hairs. Slow onset that starts with a light excitement/euphoria and a defined head high. Very focused and cerebral, giggly, and uplifted. Great for daytime and going out. Defined paranoia in certain situations, as well as heavy munchies. After the peak, a mild and energetic come down that lasts a long time. Great for depression and lethargy though not necessarily for anxiety or stress. 8/10
+        `,
+        reviewDate: "Oct 04, 2022",
+        reviewerName: "San P.",
+        reviewStars: 5,
+      },
+      {
+        reviewId: "6ba517f7-54f6-53b7-9837-18756ea1874f",
+        reviewHeading:
+          "Wake and bake for an amazing and sunny day, no matter the weather.",
+        reviewText: `
+        Strain: Super Lemon Haze Type: Sativa-dominant (80% Sativa) Genetics: Lemon Skunk x Super Silver Haze Notes: Spicy lemon smell with hazy, wispy nugs covered in hairs. Slow onset that starts with a light excitement/euphoria and a defined head high. Very focused and cerebral, giggly, and uplifted. Great for daytime and going out. Defined paranoia in certain situations, as well as heavy munchies. After the peak, a mild and energetic come down that lasts a long time. Great for depression and lethargy though not necessarily for anxiety or stress. 8/10
+        `,
+        reviewDate: "Oct 04, 2022",
+        reviewerName: "San P.",
+        reviewStars: 2,
+      },
+      {
+        reviewId: "7be2c301-f0fb-5daf-abe6-c9e0468b527b",
+        reviewHeading:
+          "Wake and bake for an amazing and sunny day, no matter the weather.",
+        reviewText: `
+        Strain: Super Lemon Haze Type: Sativa-dominant (80% Sativa) Genetics: Lemon Skunk x Super Silver Haze Notes: Spicy lemon smell with hazy, wispy nugs covered in hairs. Slow onset that starts with a light excitement/euphoria and a defined head high. Very focused and cerebral, giggly, and uplifted. Great for daytime and going out. Defined paranoia in certain situations, as well as heavy munchies. After the peak, a mild and energetic come down that lasts a long time. Great for depression and lethargy though not necessarily for anxiety or stress. 8/10
+        `,
+        reviewDate: "Oct 04, 2022",
+        reviewerName: "San P.",
+        reviewStars: 3,
+      },
+      {
+        reviewId: "a9474abc-73c8-5d01-9e50-629c4d01c739",
+        reviewHeading:
+          "Wake and bake for an amazing and sunny day, no matter the weather.",
+        reviewText: `
+        Strain: Super Lemon Haze Type: Sativa-dominant (80% Sativa) Genetics: Lemon Skunk x Super Silver Haze Notes: Spicy lemon smell with hazy, wispy nugs covered in hairs. Slow onset that starts with a light excitement/euphoria and a defined head high. Very focused and cerebral, giggly, and uplifted. Great for daytime and going out. Defined paranoia in certain situations, as well as heavy munchies. After the peak, a mild and energetic come down that lasts a long time. Great for depression and lethargy though not necessarily for anxiety or stress. 8/10
+        `,
+        reviewDate: "Oct 04, 2022",
+        reviewerName: "San P.",
+        reviewStars: 3,
+      },
+      {
+        reviewId: "7e61ed19-9158-5226-b46c-37863102d76f",
+        reviewHeading:
+          "Wake and bake for an amazing and sunny day, no matter the weather.",
+        reviewText: `
+        Strain: Super Lemon Haze Type: Sativa-dominant (80% Sativa) Genetics: Lemon Skunk x Super Silver Haze Notes: Spicy lemon smell with hazy, wispy nugs covered in hairs. Slow onset that starts with a light excitement/euphoria and a defined head high. Very focused and cerebral, giggly, and uplifted. Great for daytime and going out. Defined paranoia in certain situations, as well as heavy munchies. After the peak, a mild and energetic come down that lasts a long time. Great for depression and lethargy though not necessarily for anxiety or stress. 8/10
+        `,
+        reviewDate: "Oct 04, 2022",
+        reviewerName: "San P.",
+        reviewStars: 4,
+      },
+    ],
+  },
+};
